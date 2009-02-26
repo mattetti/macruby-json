5a8ba779c9b955a5de6add1a04e03b7a89b304a5
Matt Aimonetti <mattaimonetti@gmail.com>
2009-02-26 02:29:34 -0800

    fixed a bug in the dynlib with some escaped utf-8 characters

--- tests/runner.rb
+++ tests/runner.rb
@@ -6,3 +6,4 @@ require 'test/unit/testsuite'
 # p JSON.generate [1, 2, {"a"=>3.141}, false, true, nil, 4..10] == "[1,2,{\"a\":3.141},false,true,null,\"4..10\"]"
 
 require File.join(File.dirname(__FILE__), "test_json")
+# p JSON.load "{\"_id\":\"compound-synthesis-123562176428057\",\"_rev\":\"3225026046\",\"name\":\"Compound Synthesis\",\"sub_categories\":[\"compound-2-123562185648062\"],\"couchrest-type\":\"Category\",\"_revs_info\":[{\"rev\":\"3225026046\",\"status\":\"available\"},{\"rev\":\"4197510875\",\"status\":\"available\"},{\"rev\":\"1533305651\",\"status\":\"available\"}]}\n"
